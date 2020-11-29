b7e1ab6b034fa706ba782d7bf7863575e19c0710
armanexplorer <armanexplorer@gmail.com>
2020-11-29 18:54:04 +0330

    final2

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 import random
 
-TOTAL_TURNS = 5
-ROW_NUMBER = 10
-COL_NUMBER = 10
+TOTAL_TURNS = 1
+ROW_NUMBER = 4
+COL_NUMBER = 20
 bit_array = list()
 colors = [0,1]      # 0 is grey and 1 is blue
+init_answers = []
 for i in range(TOTAL_TURNS):
     l = list()
+    s = 0
     for j in range(ROW_NUMBER):
-        l.append(random.choices(colors, weights=[1,1], k=COL_NUMBER))
+        ans_num_in_row = random.choice(range(5,10))
+        s += ans_num_in_row
+        l.append(random.choices(colors, weights=[COL_NUMBER-ans_num_in_row,ans_num_in_row], k=COL_NUMBER))
     bit_array.append(l)
+    init_answers.append(s)
 
-print(bit_array[0])
-print(bit_array[1])
+print(init_answers)
+print()
+print(bit_array)
